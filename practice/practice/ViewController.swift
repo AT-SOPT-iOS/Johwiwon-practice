a572524c746252e2a177171fd76799592ea44d01
hwidung <hwhwhw74@naver.com>
2025-04-10 21:51:09 +0900

    feat: 프로젝트 세팅

---
+++ practice/practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by 조휘원 on 4/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
